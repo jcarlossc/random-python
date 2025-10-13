5642dbcc117d01a45dead36969ddf683f96c57a6
jcarlossc <josecscosta@fac.pe.senac.br>
2025-10-13 20:05:36 -0300

    Método gera_float.

---
+++ src/random_python/main.py
@@ -0,0 +1,21 @@
+from random_python.ClasseAleatoria import ClasseAleatoria
+
+def main():
+    print('\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
+    print(f' ESTUDO SOBRE O MÓDULO RANDOM EM LINGUAGEM PYTHON')
+    print('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
+
+    aleatorio = ClasseAleatoria()
+    print(f'Gera float entre 0 e 1: {aleatorio.gera_float():.2f}')
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
