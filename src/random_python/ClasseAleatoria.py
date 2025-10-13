045e29567f226a51855e96bc13a3da76f3c59672
jcarlossc <josecscosta@fac.pe.senac.br>
2025-10-13 20:17:36 -0300

    Método gera_inteiro_entre.

--- src/random_python/ClasseAleatoria.py
+++ src/random_python/ClasseAleatoria.py
@@ -9,3 +9,6 @@ class ClasseAleatoria:
     
     def gera_float_entre(self, num1: float, num2: float) -> float:
         return random.uniform(num1, num2)
+    
+    def gera_inteiro_entre(self, num1: int, num2: int) -> int:
+        return random.randint(num1, num2)
